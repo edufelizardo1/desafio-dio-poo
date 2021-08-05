9210d30914128149fb6a49229254cdf7e66d8514
Eduardo Felizardo <edufelizardo1@gmail.com>
2021-08-05 16:06:23 -0300

    final

--- src/main.java
+++ src/main.java
@@ -1,6 +1,4 @@
-import br.com.dio.desafio.dominio.Conteudo;
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -23,8 +21,38 @@ public class main {
         mentoria.setDescricao("Mentoria: Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev dev = new Dev();
+        dev.setNome("Eduardo");
+        dev.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos Eduardo" + dev.getConteudosInscritos());
+        dev.progredir();
+        dev.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos inscritos Eduardo" + dev.getConteudosInscritos());
+        System.out.println("Conteúdos inscritos Eduardo" + dev.getConteudosConcluidos());
+        System.out.println("XP: " + dev.calcularTotalXP());
+
+        System.out.println("-----");
+
+        Dev dev1 = new Dev();
+        dev.setNome("Camila");
+        dev1.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos Camila" + dev1.getConteudosInscritos());
+        dev1.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos inscritos Camila" + dev1.getConteudosInscritos());
+        System.out.println("Conteúdos inscritos Camila" + dev1.getConteudosConcluidos());
+        System.out.println("XP: " + dev1.calcularTotalXP());
     }
 }
