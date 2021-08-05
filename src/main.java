292f05afb9138fcec498515806d123b304f1e573
Eduardo Felizardo <edufelizardo1@gmail.com>
2021-08-05 15:21:06 -0300

    create Conteudo

--- src/main.java
+++ src/main.java
@@ -1,3 +1,4 @@
+import br.com.dio.desafio.dominio.Conteudo;
 import br.com.dio.desafio.dominio.Curso;
 import br.com.dio.desafio.dominio.Mentoria;
 
@@ -14,6 +15,8 @@ public class main {
         curso2.setTitulo("Curso JavaScript");
         curso2.setDescricao("Descrição: Curso JavaScript");
         curso2.setCargaHoraria(8);
+
+        Conteudo conteudo = new Curso();
 
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("mentoria Java");
